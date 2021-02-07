c80d3a3d437e953a521d0402f7908452850d9535
foday <foday_s@yahoo.com>
2021-02-07 09:03:21 -0600

    The enroll method - Enroll in Courses
    A 'do' loop is created - The Java do-while loop is used to iterate a part of the program several times. If the number of iteration is not fixed and you must have to execute the loop at least once, it is recommended to use do-while loop. The Java do-while loop is executed at least once because condition is checked after loop body.
    Inside the loop Students are instructed to enter the course to enroll in then the Scanner System.in method gets the input and moves to the next line after student press enters due to the 'in.nextLine()' method

--- StudentManagementSystem/Student.java
+++ StudentManagementSystem/Student.java
@@ -5,11 +5,12 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int StudentID;
-    private String courses;
-    private int tutionBalance;
+    private int studentID;
+    private String courses = null;
+    private int tuitionBalance = 0;
     private int costOfCourse = 600;
     private static int id = 1001;
+    
     
     // Constructors - prompts students to enter name and year 
     public Student() {
@@ -22,15 +23,44 @@ public class Student {
         
         System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class Level: ");
         this.gradeYear = in.nextInt();
-        System.out.println(firstName + " " + lastName + " " + gradeYear);
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+        setStudentID();
 
     }
     
     // Generate an ID
+    private void setStudentID() {
+        // Grade Level + ID
+        id++;
+        this.studentID = gradeYear + id;
+
+    }
 
     // Enroll in Courses
+    public void enroll() {
+        // Get inside a loop, user hits 0
+        do {
+            System.out.println("Enter course to enroll (0 to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (course != "Q") {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+        }
+        else { 
+            System.out.println("BREAK!");
+            break;
+        }
+    } while (1 != 0);
+    
+    System.out.println("ENROLLED IN: " + courses);
+    System.out.println("TUITION BALANCE: " + tuitionBalance);
 
-    //Pay Tuition
+}
 
+    // Pay Tuition
+
+    
     //Show status 
 }
--- StudentManagementSystem/StudentDatabaseApp.java
+++ StudentManagementSystem/StudentDatabaseApp.java
@@ -2,6 +2,9 @@ package StudentManagementSystem;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+        Student stu1 = new Student();
+        stu1.enroll();
+        
         // ASk how many new users we want to add 
 
         // Create a member of new students
