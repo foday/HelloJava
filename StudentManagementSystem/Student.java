8c0df4c750a5ade6a8eadfd9fefc0a71ff263ade
foday <foday_s@yahoo.com>
2021-02-06 18:41:53 -0600

    Class, objects, and constructor
    The student class or the cookie cutter is not an object and is empty and holds no space or memory but when the student cookie cutter is placed on the dough the constructor is activated and the student object is created. An object is any entity that has state and behavior. A state is not static it is something that changes. Static is for classes not for objects. A class are templates that are used to create objects and to define object data types and methods. A constructor is a block of codes similar to the method. It is called when an instnce of the object is created, and memory is allocated for the object. It is a special type of method which is used to initialize the object. A constructor is created at compile time and a written constructor is not needed because Java will automatically make one for the developer if one is not written. The constructor automatically created is the one with no parameter and it is default

--- StudentManagementSystem/Student.java
+++ StudentManagementSystem/Student.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Student {
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int StudentID;
     private String courses;
     private int tutionBalance;
@@ -13,6 +13,15 @@ public class Student {
     // Constructors - prompts students to enter name and year 
     public Student() {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter the student's first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Enter student's last name: ");
+        this.lastName = in.nextLine();
+        
+        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class Level: ");
+        this.gradeYear = in.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
 
     }
     
